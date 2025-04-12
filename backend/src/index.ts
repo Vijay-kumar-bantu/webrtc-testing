51bfef8133724668f4a92d48221f73b688a3f124
Vijay <capvijju24@gmail.com>
2025-04-12 11:37:41 +0530

    intiaite repo

---
+++ backend/src/index.ts
@@ -0,0 +1,51 @@
+import { WebSocket, WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+console.log("WebSocket server started on ws://localhost:8080");
+
+let senderSocket: null | WebSocket = null;
+let receiverSocket: null | WebSocket = null;
+
+wss.on("connection", function connection(ws) {
+	ws.on("error", console.error);
+
+	ws.on("message", function message(data: any) {
+		const message = JSON.parse(data);
+		if (message.type === "sender") {
+			console.log("sender added");
+			senderSocket = ws;
+		} else if (message.type === "receiver") {
+			console.log("receiver added");
+			receiverSocket = ws;
+		} else if (message.type === "createOffer") {
+			if (ws !== senderSocket) {
+				return;
+			}
+			console.log("sending offer");
+			receiverSocket?.send(
+				JSON.stringify({ type: "createOffer", sdp: message.sdp })
+			);
+		} else if (message.type === "createAnswer") {
+			if (ws !== receiverSocket) {
+				return;
+			}
+			console.log("sending answer");
+			senderSocket?.send(
+				JSON.stringify({ type: "createAnswer", sdp: message.sdp })
+			);
+		} else if (message.type === "iceCandidate") {
+			if (ws === senderSocket) {
+				console.log("sender sending ice candidate");
+				receiverSocket?.send(
+					JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+				);
+			} else if (ws === receiverSocket) {
+				console.log("receiver sending ice candidate");
+				senderSocket?.send(
+					JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+				);
+			}
+		}
+	});
+});
