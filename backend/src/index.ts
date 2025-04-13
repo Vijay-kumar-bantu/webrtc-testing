f7aa89179582fb3ce29942b43ceeef304e886b84
Vijay <capvijju24@gmail.com>
2025-04-13 23:27:14 +0530

    updated backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,40 +10,60 @@ let receiverSocket: null | WebSocket = null;
 wss.on("connection", function connection(ws) {
 	ws.on("error", console.error);
 
+	ws.on("close", () => {
+		if (ws === senderSocket) {
+			senderSocket = null;
+		} else if (ws === receiverSocket) {
+			receiverSocket = null;
+		}
+	});
+
+	if (senderSocket) {
+		receiverSocket = ws;
+		ws.send(JSON.stringify({ type: "start" }));
+		senderSocket.send(JSON.stringify({ type: "start" }));
+	} else {
+		senderSocket = ws;
+	}
+
 	ws.on("message", function message(data: any) {
 		const message = JSON.parse(data);
-		if (message.type === "sender") {
-			console.log("sender added");
-			senderSocket = ws;
-		} else if (message.type === "receiver") {
-			console.log("receiver added");
-			receiverSocket = ws;
-		} else if (message.type === "createOffer") {
+
+		if (message.type === "createOffer") {
 			if (ws !== senderSocket) {
-				return;
+				senderSocket?.send(
+					JSON.stringify({ type: "createOffer", sdp: message.sdp })
+				);
+			} else {
+				receiverSocket?.send(
+					JSON.stringify({ type: "createOffer", sdp: message.sdp })
+				);
 			}
-			console.log("sending offer");
-			receiverSocket?.send(
-				JSON.stringify({ type: "createOffer", sdp: message.sdp })
-			);
 		} else if (message.type === "createAnswer") {
 			if (ws !== receiverSocket) {
-				return;
+				receiverSocket?.send(
+					JSON.stringify({ type: "createAnswer", sdp: message.sdp })
+				);
+			} else {
+				senderSocket?.send(
+					JSON.stringify({ type: "createAnswer", sdp: message.sdp })
+				);
 			}
-			console.log("sending answer");
-			senderSocket?.send(
-				JSON.stringify({ type: "createAnswer", sdp: message.sdp })
-			);
 		} else if (message.type === "iceCandidate") {
 			if (ws === senderSocket) {
-				console.log("sender sending ice candidate");
 				receiverSocket?.send(
-					JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+					JSON.stringify({
+						type: "iceCandidate",
+						peer: message.peer,
+						candidate: message.candidate,
+					})
 				);
 			} else if (ws === receiverSocket) {
-				console.log("receiver sending ice candidate");
 				senderSocket?.send(
-					JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+					JSON.stringify({
+						type: "iceCandidate",
+						candidate: message.candidate,
+					})
 				);
 			}
 		}
